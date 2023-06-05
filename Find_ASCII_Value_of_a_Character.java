fd7035326ef30a083892127ed11ab60b8c5d6e1a
padmadivyapothula <119720502+padmadivyapothula@users.noreply.github.com>
2023-06-05 18:09:57 +0530

    import java.util.*; public class ASC {     public static void main(String a[])     {         char ch;         Scanner scan = new Scanner(System.in);         ch = scan.next().charAt(0);         scan.close();         int c = ch;         System.out.print(c);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ASC
+{
+    public static void main(String a[])
+    {
+        char ch;
+        Scanner scan = new Scanner(System.in);
+        ch = scan.next().charAt(0);
+        scan.close();
+        int c = ch;
+        System.out.print(c);
+    }
+}
