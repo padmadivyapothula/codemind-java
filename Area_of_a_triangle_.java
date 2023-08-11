9ba284eaa59779af877b4184826dd345f7e09a2b
padmadivyapothula <119720502+padmadivyapothula@users.noreply.github.com>
2023-08-11 21:17:41 +0530

    import java.util.*; public class cross {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=(a*b)/2;         System.out.print(c);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class cross
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=(a*b)/2;
+        System.out.print(c);
+    }
+}
