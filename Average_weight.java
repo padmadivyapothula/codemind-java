b3190c9800fd49cde4cd38863e739946996dbaab
padmadivyapothula <119720502+padmadivyapothula@users.noreply.github.com>
2023-08-11 20:47:14 +0530

    import java.util.*; public class avg { public static void main(String args[])
        {
            Scanner sc = new Scanner(System.in);
            int a,b,c,d;
            a=sc.nextInt();
            b=sc.nextInt();
            c=sc.nextInt();
            d=3*a-b-c;
            System.out.print(d);
        }
    }

---
+++ Average_weight.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class avg
+{
+public static void main(String args[])
+
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=3*a-b-c;
+        System.out.print(d);
+    }
+}
